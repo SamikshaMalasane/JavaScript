62e2e8d02f11b0c9b672fddb3c6e084386e29c0e
SamikshaMalasane <samikshamalasane93@gmail.com>
2024-04-03 16:19:27 +0530

    This is javascript code for practice

---
+++ practice.js
@@ -0,0 +1,106 @@
+/*Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+
+let a = 20;
+let b = 10;
+
+let sum = a + b;
+let difference = a-b;
+let product = a*b;
+let quotient = a/b;
+let modulus = a%b;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+console.log(modulus);*/
+
+
+/*Take two numbers and only print the integer part when a is divided by b
+let a = 25;
+let b= 3;
+let integer = Math.floor(a/b);
+console.log(integer);*/
+
+
+/* Take two numbers a,b and print the exponential power of the first number raised by the second.
+let a = 5;
+let b = 2;
+let power = a**b;
+console.log(power);*/
+
+/*Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+let a = 20;
+let b = 3;
+let quotient = 20/3;
+let integer = Math.floor(20/3);
+console.log(quotient-integer);*/
+
+/*Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+let a = 50;
+let b = 75;
+let c = 67;
+let d = 25;
+let e = 38;
+let sum = a + b + c + d + e;
+console.log(sum/5);*/
+
+
+/*Take the cost price a and selling price b of a product, and print the profit obtained on the product
+let cost_price = 50;
+let selling_price = 65;
+let profit = selling_price - cost_price;
+console.log(profit);*/
+
+
+/*Take a number and print out the last digit of the number.
+let num =12345;
+let lastdigit = num % 10;
+console.log(lastdigit);*/
+
+
+
+/*Take a 2 digit number and return the first and last digits.
+let num = 12345;
+let firstDigit = Math.floor(num/10000);
+let lastDigit = num % 10;
+console.log(firstDigit);
+console.log(lastDigit);*/
+
+
+
+
+/*Write a JavaScript program to find the largest of three numbers.
+let a = 2;
+let b = 10;
+let c = 8;
+
+let largest;
+if (a>=b && a>=c){
+    largest = a;
+}
+else if(b>=a && b>=c){
+    largest = b;
+}
+else{
+    largest = c
+}
+console.log(largest);*/
+
+
+
+/*Write a JavaScript program to check whether a number is even or odd.
+let num = 24;
+if (num % 2 ===0){
+    console.log(num + "is even")
+}
+else{
+    console.log(num + "is odd");
+}*/
+
+
+
+/*Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+let celsius = 100;
+let fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit);*/
